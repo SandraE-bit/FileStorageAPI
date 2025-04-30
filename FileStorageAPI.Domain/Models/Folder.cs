986c747f1a04e8dffb082d891f67e29b2310cb1e
SandraE-bit <sandra.ernas@outlook.com>
2025-04-30 18:20:06 +0200

    localHost

--- FileStorageAPI.Domain/Models/Folder.cs
+++ FileStorageAPI.Domain/Models/Folder.cs
@@ -1,9 +1,15 @@
 ﻿public class Folder
 {
-    public string UserId { get; set; } = string.Empty;
-    public Guid? ParentFolderId { get; set; }
-    public Folder? ParentFolder { get; set; }
-    public AppUser? User { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
+
     public string Name { get; set; } = string.Empty;
-   
+
+    public string UserId { get; set; } 
+
+    public List<FileItem> Files { get; set; } = new();
+
+    public List<Folder> SubFolders { get; set; } = new(); 
+
+    public Guid? ParentFolderId { get; set; } 
+    public Folder? ParentFolder { get; set; }
 }
